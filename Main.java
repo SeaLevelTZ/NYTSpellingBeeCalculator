5f4f4ceb3f8bf41b71d27d30a4cab18c042a3b6a
SeaLevelTZ <155255404+SeaLevelTZ@users.noreply.github.com>
2024-03-04 13:19:01 -0500

    .

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
 import java.util.Scanner;
+import java.io.File;
+import java.util.ArrayList;
 public class Main{
     public static void main(String[]args){
     	Scanner scan = new Scanner(System.in);
-    	
+    	ArrayList<String> wordArr = new ArrayList<String>();
+        
     }
 }
