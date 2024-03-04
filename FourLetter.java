4fc635d6c20038b07ae749072e1250b1ce8ac657
Villem11 <olmsteadv5@gmail.com>
2024-03-04 17:06:49 -0500

    txt to arrayList

--- FourLetter.java
+++ FourLetter.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 public class FourLetter {
-    ArrayList<String> list = new ArrayList<>();
+    public static void main(String[] args) throws FileNotFoundException {
+    ArrayList<String> wordList = new ArrayList<>();
     Scanner scanner = new Scanner(new File("words.txt"));
     while(scanner.hasNextLine()){
-        list.add(scanner.nextLine());
+        wordList.add(scanner.nextLine());
     }
+    scanner.close();
 }
-    /*more than 4 letters
-    //more than 7 different charcters. 
+}
