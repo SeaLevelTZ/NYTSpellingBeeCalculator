5044705ff4fc60c5d68772418a9852a5c72731ae
SeaLevelTZ <155255404+SeaLevelTZ@users.noreply.github.com>
2024-03-04 17:57:20 -0500

    added stuff but not working

--- FourLetter.java
+++ FourLetter.java
@@ -3,12 +3,21 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 public class FourLetter {
-    public static void main(String[] args) throws FileNotFoundException {
-    ArrayList<String> wordList = new ArrayList<>();
-    Scanner scanner = new Scanner(new File("words.txt"));
-    while(scanner.hasNextLine()){
-        wordList.add(scanner.nextLine());
+    public static ArrayList<String> makeList(){// throws FileNotFoundException 
+        ArrayList<String> wordList = new ArrayList<>();
+        
+        try{
+        Scanner scanner = new Scanner(new File("words.txt"));
+        while(scanner.hasNextLine()){
+            wordList.add(scanner.nextLine());
+        }
+        scanner.close();
+        
+    }catch (FileNotFoundException e) {
+        System.out.println("An error occurred.");
+        e.printStackTrace();
+      }
+      return wordList;
     }
-    scanner.close();
-}
+    
 }
--- Main.java
+++ Main.java
@@ -5,6 +5,36 @@ public class Main{
     public static void main(String[]args){
     	Scanner scan = new Scanner(System.in);
     	ArrayList<String> wordArr = new ArrayList<String>();
-        
+        String other6 = scan.nextLine();
+        String a = scan.nextLine();
+        ArrayList<String> allWords = FourLetter.makeList();
+        for(String i : allWords){
+            if(i.length()>3&&isInWord(a, i, 1) && isInWord(other6, i, 0)){
+                wordArr.add(i);
+            }
+        }
+        System.out.println(wordArr);
     }
+    private static boolean isInWord(String letters, String word, int type){
+        int count = 0;
+        for(int i = 0;i<letters.length();i++){
+            boolean isCounted = false;
+            for(int j = 0; j<word.length();j++){
+                if(!isCounted && word.substring(j, j+1).equals(letters.substring(i, i+1))){
+                    count++;
+                    isCounted = true;
+                }
+            }
+        }
+        if(type ==1){
+            if(count==1) return true;
+            else return false;
+        }
+        else{
+            if(count==7) return true;
+            else return false;
+        }
+    }
+
+
 }
