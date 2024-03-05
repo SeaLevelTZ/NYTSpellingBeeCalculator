8ff622d49ce7c74e85112cea88900f94abd562b3
SeaLevelTZ <155255404+SeaLevelTZ@users.noreply.github.com>
2024-03-04 22:37:10 -0500

    better wordlist from umich

--- Main.java
+++ Main.java
@@ -6,14 +6,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         ArrayList<String> wordArr = new ArrayList<String>();
-        String other6 = scan.nextLine();
+        System.out.print("Enter center letter: ");
         String a = scan.nextLine();
+        System.out.print("Enter Other letters: ");
+        String other6 = scan.nextLine();
         ArrayList<String> allWords = FourLetter.makeList();
         for (String i : allWords) {
             if (i.length() > 3 && hasCenter(a, i) && isInWord(other6, i, a)) {
                 wordArr.add(i);
             }
         }
+        System.out.println(wordArr.size() + " words found\n");
         System.out.println(wordArr);
     }
 
