752b0c875246e2451d166d9b59e74b6ec5fe0d65
SeaLevelTZ <155255404+SeaLevelTZ@users.noreply.github.com>
2024-03-06 22:04:40 -0500

    merged file

--- Main.java
+++ Main.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
 
-        ArrayList<String> wordList = FourLetter.makeList();
+        ArrayList<String> wordList = makeList();
 
         Scanner scan = new Scanner(System.in);
         ArrayList<String> wordArr = new ArrayList<String>();
@@ -13,7 +14,7 @@ public class Main {
         String a = scan.nextLine();
         System.out.print("Enter Other letters: ");
         String other6 = scan.nextLine();
-        ArrayList<String> allWords = FourLetter.makeList();
+        ArrayList<String> allWords = makeList();
         for (String i : allWords) {
             if ( hasCenter(a, i) && isInWord(other6, i, a)) {
                 wordArr.add(i);
@@ -38,4 +39,40 @@ public class Main {
             return false;
         return true;
     }
+    public static ArrayList<String> makeList(){
+        ArrayList<String> wordList = new ArrayList<>();
+        
+        try{
+        Scanner scanner = new Scanner(new File("words.txt"));
+        while (scanner.hasNextLine()) {
+            String word = scanner.nextLine();
+            if (isValidWord(word)) {
+                wordList.add(word);
+        }
+    }
+        scanner.close();
+        
+    }catch (FileNotFoundException e) {
+        System.out.println("An error occurred.");
+        e.printStackTrace();
+      }
+      return wordList;
+    }
+
+private static boolean isValidWord(String str){
+    if(str.length()<=3) 
+    return false; 
+    ArrayList<String> validLetter = new ArrayList<>();
+    int count = 0;
+        for(int i = 0; i<str.length(); i++){
+            for(String x: validLetter){
+                if(x.compareTo(str.substring(i, i+1))!=0)
+                count++;
+            }
+
+        }
+    if(count > 7)
+    return false; 
+    return true;
+}
 }
