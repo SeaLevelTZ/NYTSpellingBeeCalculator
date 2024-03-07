8b353945bd25f64d5f711797f62bb79be9bf8ded
SeaLevelTZ <155255404+SeaLevelTZ@users.noreply.github.com>
2024-03-06 22:15:38 -0500

    performance improvement, reduced length of code

--- Main.java
+++ Main.java
@@ -5,20 +5,25 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-
-        ArrayList<String> wordList = makeList();
-
         Scanner scan = new Scanner(System.in);
         ArrayList<String> wordArr = new ArrayList<String>();
         System.out.print("Enter center letter: ");
         String a = scan.nextLine();
         System.out.print("Enter Other letters: ");
         String other6 = scan.nextLine();
-        ArrayList<String> allWords = makeList();
-        for (String i : allWords) {
-            if ( hasCenter(a, i) && isInWord(other6, i, a)) {
-                wordArr.add(i);
+        try {
+            Scanner scanner = new Scanner(new File("wwords.txt"));
+            while (scanner.hasNextLine()) {
+                String i = scanner.nextLine();
+                if (i.length()>3&& hasCenter(a, i) && isInWord(other6, i, a)) {
+                    wordArr.add(i);
+                }
             }
+            scanner.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
         }
         System.out.println(wordArr.size() + " words found\n");
         System.out.println(wordArr);
@@ -39,40 +44,11 @@ public class Main {
             return false;
         return true;
     }
-    public static ArrayList<String> makeList(){
+
+    public static ArrayList<String> makeList() {
         ArrayList<String> wordList = new ArrayList<>();
-        
-        try{
-        Scanner scanner = new Scanner(new File("words.txt"));
-        while (scanner.hasNextLine()) {
-            String word = scanner.nextLine();
-            if (isValidWord(word)) {
-                wordList.add(word);
-        }
-    }
-        scanner.close();
-        
-    }catch (FileNotFoundException e) {
-        System.out.println("An error occurred.");
-        e.printStackTrace();
-      }
-      return wordList;
-    }
 
-private static boolean isValidWord(String str){
-    if(str.length()<=3) 
-    return false; 
-    ArrayList<String> validLetter = new ArrayList<>();
-    int count = 0;
-        for(int i = 0; i<str.length(); i++){
-            for(String x: validLetter){
-                if(x.compareTo(str.substring(i, i+1))!=0)
-                count++;
-            }
-
-        }
-    if(count > 7)
-    return false; 
-    return true;
-}
+        
+        return wordList;
+    }
 }
